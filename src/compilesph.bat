daeb5431f780c6bc955539bd826a804dcd244208
Edo Potato <edward_yang_125@hotmail.com>
2021-11-17 20:13:05 +1100

    first commit to repo

--- src/compilesph.bat
+++
@@ -1,3 +0,0 @@
-ifort /traceback /O3 /Qxhost /Qipo param.f90 arrayAlloc.f90 art_visc.f90 density.f90 ext_force.f90 flink_lisk.f90 input.f90 int_force.f90 integration.f90 kernel.f90 main_sph.f90 output.f90 single_step.f90 time_print.f90 virt_part.f90 /exe:runsph.exe
-
-del *.obj *.mod
