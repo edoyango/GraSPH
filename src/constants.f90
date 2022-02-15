b6781dcc2bd3a08843a7a3737da50266c472366c
Edo Potato <edward_yang_125@hotmail.com>
2022-02-15 20:31:16 +1100

    adding public

--- src/constants.f90
+++ src/constants.f90
@@ -1,6 +1,6 @@
 module constants
 
 	! pi, g (gravity)
-	real(kind(1d0)),parameter:: pi = 3.14159265358979323846d0, g = 9.81d0
+	real(kind(1d0)),parameter,public:: pi = 3.14159265358979323846d0, g = 9.81d0
 	
 end module constants
