bcd38236e1a03174ee12a4595582f8f105f007af
FishChickenFace <edward_yang_125@hotmail.com>
2022-06-10 19:13:07 +1000

    Adding periods

--- src_serial/kernel_m.f90
+++ src_serial/kernel_m.f90
@@ -20,22 +20,22 @@ contains
         
         q = r/thsml
         tw = 0_f
-        tdwdx(:) = 0_f
+        tdwdx(:) = 0._f
         
         SELECT CASE (SKF)
             CASE (1) ! cubic
-                if (dims==2) factor = 10_f/(7_f*pi*thsml*thsml)
-                if (dims==3) factor = 1_f/(pi*thsml*thsml)
+                if (dims==2) factor = 10._f/(7._f*pi*thsml*thsml)
+                if (dims==3) factor = 1._f/(pi*thsml*thsml)
                 tw = factor*(0.25_f*DIM(2._f,q)**3 - DIM(1._f,q)**3)
                 tdwdx = -factor*3._f*(0.25_f*DIM(2._f,q)**2 - DIM(1._f,q)**2)*dx(:)/(r*thsml)
             CASE (2) ! quartic
-                if (dims==2) factor = 96_f/(1199_f*pi*thsml*thsml)
-                if (dims==3) factor = 1_f/(20_f*pi*thsml*thsml*thsml)
-                tw = factor*(DIM(2.5_f,q)**4 - 5_f*DIM(1.5_f,q)**4 + 10_f*DIM(0.5_f,q)**4)
-                tdwdx(:) = -factor*4._f*(DIM(2.5_f,q)**3 - 5_f*DIM(1.5_f,q)**3 + 10_f*DIM(0.5_f,q)**3)*dx(:)/(r*thsml)
+                if (dims==2) factor = 96._f/(1199._f*pi*thsml*thsml)
+                if (dims==3) factor = 1._f/(20._f*pi*thsml*thsml*thsml)
+                tw = factor*(DIM(2.5_f,q)**4 - 5._f*DIM(1.5_f,q)**4 + 10._f*DIM(0.5_f,q)**4)
+                tdwdx(:) = -factor*4._f*(DIM(2.5_f,q)**3 - 5._f*DIM(1.5_f,q)**3 + 10._f*DIM(0.5_f,q)**3)*dx(:)/(r*thsml)
             CASE (3) ! quintic
-                if (dims==2) factor = 7_f/(478_f*pi*thsml*thsml)
-                if (dims==3) factor = 1_f/(120_f*pi*thsml*thsml*thsml)
+                if (dims==2) factor = 7._f/(478._f*pi*thsml*thsml)
+                if (dims==3) factor = 1._f/(120._f*pi*thsml*thsml*thsml)
                 tw = factor*(DIM(3._f,q)**5 - 6._f*DIM(2._f,q)**5 + 15._f*DIM(1._f,q)**5)
                 tdwdx(:) = -factor*5._f*(DIM(3._f,q)**4 - 6._f*DIM(2._f,q)**4 + 15._f*DIM(1._f,q)**4)*dx(:)/(r*thsml)
             CASE (4) ! Wenland Quintic C2
@@ -54,10 +54,10 @@ contains
                 tw = factor*DIM(2._f,q)**8*(16._f*q**3 + 25_f*q**2 + 16._f*q + 4._f)
                 tdwdx(:) = -factor*22._f*q*(8._f*q**2 + 7._f*q + 2._f)*DIM(2._f,q)**7*dx(:)/(r*thsml)
             CASE (7) ! gaussian
-                factor = 1_f/(thsml**dims*pi**(0.5_f*dims))
-                if (q.ge.0_f .and. q.le.3_f) then
+                factor = 1._f/(thsml**dims*pi**(0.5_f*dims))
+                if (q.ge.0._f .and. q.le.3._f) then
                     tw = factor*exp(-q*q)
-                    tdwdx(:) = tw*2_f*dx(:)/(thsml*thsml)
+                    tdwdx(:) = tw*2._f*dx(:)/(thsml*thsml)
                 end if
             
         END SELECT
@@ -74,11 +74,11 @@ contains
         
         select case (skf)
             case (1,4,5,6)
-                scale_k = 2_f
+                scale_k = 2._f
             case (2)
                 scale_k = 2.5_f
             case (3,7)
-                scale_k = 3_f
+                scale_k = 3._f
         end select
     
     end function kernel_k
