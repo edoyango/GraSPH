de0ba4f2dd79695fcd021ad057b940c903521476
Edward <edward_yang_125@hotmail.com>
2022-10-07 23:04:51 +1100

    first attempt

--- src_GPU/csort.cu
+++ src_GPU/csort.cu
@@ -1,12 +1,7 @@
 #include <thrust/device_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/sort.h>
-#include <thrust/execution_policy.h>
-
-typedef struct parttype {
-	int i, itype;
-	float rho, x[3], v[3], sig[6];
-} parttype;	
+#include <thrust/execution_policy.h>	
 
 extern "C" {
 	
@@ -14,13 +9,6 @@ extern "C" {
 	{
 		thrust::device_ptr<int> dev_ptr_keys(keys);
 		thrust::device_ptr<int> dev_ptr_values(values);
-		thrust::sort_by_key(thrust::device, dev_ptr_keys, dev_ptr_keys+N, dev_ptr_values);
-	}
-	
-	void sortbykey_pad_wrapper( int *keys, parttype *values, int N)
-	{
-		thrust::device_ptr<int> dev_ptr_keys(keys);
-		thrust::device_ptr<parttype> dev_ptr_values(values);
 		thrust::sort_by_key(thrust::device, dev_ptr_keys, dev_ptr_keys+N, dev_ptr_values);
 	}
 	
