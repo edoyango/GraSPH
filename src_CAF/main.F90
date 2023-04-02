b3cef6a3b1452827ffc4491518b98f2ec3054e03
Edward Yang <edward_yang_125@hotmail.com>
2023-04-02 17:33:47 +1000

    Add a kind of working flaps example

--- src_CAF/main.F90
+++ src_CAF/main.F90
@@ -8,6 +8,7 @@ program SPH
    use summary_m, only: preamble, print_summary
    use time_integration_m, only: time_integration
    use output_m, only: output
+   use flap, only: command_line_interface
 
    implicit none
    integer:: thisImage, numImages, maxtimestep, print_step, save_step, ntotal, nvirt, maxnloc, maxinter, i
@@ -17,13 +18,46 @@ program SPH
    type(particles), allocatable, codimension[:]:: parts(:)
    type(interactions), allocatable:: pairs(:)
    type(time_tracking):: timings
+   type(command_line_interface):: argparse
+      integer:: argerror
+
+      call argparse%init(description='potato')
+      call argparse%add(switch='--maxtimestep', &
+                        switch_ab='-m', &
+                        help='Max number of time-steps to run the simulation for.', &
+                        required=.true., &
+                        act='store', &
+                        error=argerror)
+           
+      call argparse%add(switch='--printtimestep', &
+                        switch_ab='-p', &
+                        help='Interval of time-steps to write to terminal e.g., -p 1000 will print information every ' &
+                           //'1000 time-steps.', &
+                        required=.true., &
+                        act='store', &
+                        error=argerror)
+
+      call argparse%add(switch='--savetimestep', &
+                        switch_ab='-s', &
+                        help='Interval of time-steps to save data e.g., -s 1000 will save output data every 1000 ' &
+                           //'time-steps. Data is saved to <output-directory>/sph_out*.h5.', &
+                        required=.true., &
+                        act='store', &
+                        error=argerror)
+
+      call argparse%get(switch='-m', val=maxtimestep, error=argerror)
+      call argparse%get(switch='-p', val=print_step, error=argerror)
+      call argparse%get(switch='-s', val=save_step, error=argerror)
+
+      write(*,*) maxtimestep
+      stop
 
    ! Saving image id and total number of images
    thisImage = this_image()
    numImages = num_images()
 
    ! Printing preamble to screen
-   call preamble(thisImage, numImages, maxtimestep, print_step, save_step)
+   ! call preamble(thisImage, numImages, maxtimestep, print_step, save_step)
 
    ! Retrieving kernel k parameter for use in the program
    scale_k = kernel_k(skf)
