4ceb044d8a63f3f6d2cedfd494eb74ab0f6ddfd7
Edward Yang <edward_yang_125@hotmail.com>
2025-10-13 17:01:22 +1100

    implement crude morris boundary
    Has core functionality of morris boundary,
    but assumes that everything except x, v are
    copied from the real particle. Probably not
    correct. Also needs to be made more generic
    and performant.

--- app/dambreak-ghost-morris-boundary.f90
+++ app/dambreak-ghost-morris-boundary.f90
@@ -16,51 +16,57 @@ module grasph_dambreak_ghost_morris_boundary_m
     real(fp), parameter:: dx = 0.5_fp, g = -9.81_fp, rho0 = 1000._fp
 
     ! define how fluid particles interact with boundary
-    type, extends(base_sweeper_t):: boundary_update_sweeper_t
+    type, extends(fluid_sweeper_t):: morris_boundary_sweeper_t
+        real(fp):: x
     contains
-        procedure:: sweep => boundary_update_sweep
-    end type boundary_update_sweeper_t
+        procedure:: sweep => morris_boundary_sweep
+    end type morris_boundary_sweeper_t
 
 contains
 
-    subroutine boundary_update_sweep(self, pairs, psys_lhs, psys_rhs)
-        class(boundary_update_sweeper_t), intent(in):: self
+    subroutine morris_boundary_sweep(self, pairs, psys_lhs, psys_rhs)
+        class(morris_boundary_sweeper_t), intent(in):: self
         type(particle_pairs_t), intent(in):: pairs
         class(particle_system_t), intent(inout):: psys_lhs
         class(particle_system_t), optional, intent(inout):: psys_rhs
         integer:: i, j, k
-        real(fp):: mw, vw
-        real(fp), allocatable:: wsum(:)
+        class(eos_particle_t), pointer:: ps_fluid(:)
+        real(fp):: dummy_drhodt, dummy_dvxdt(ndims), vb(ndims), da, db
 
-        if (present(psys_rhs)) then
-            allocate (wsum(psys_rhs%size), source=0._fp)
-        else
+        if (.not. present(psys_rhs)) then
             error stop "Expected psys_rhs to be passed in."
         end if
 
-        do i = 1, psys_rhs%size
-            psys_rhs%particles(i)%rho = 0._fp
-            psys_rhs%particles(i)%v(:) = 0._fp
-        end do
+        select type (ps => psys_lhs%particles)
+        class is (eos_particle_t)
+            ps_fluid => ps
+        class default
+            error stop "Expected psys_lhs%particles to be eos_particle_t."
+        end select
 
+        ! perform sweep
         do k = 1, pairs%npairs_total
             i = pairs%pair_ij(1, k)
             j = pairs%pair_ij(2, k)
-            mw = psys_lhs%particles(i)%mass*pairs%w(k)
-            vw = mw/psys_lhs%particles(i)%rho
-            wsum(j) = wsum(j) + vw
-            psys_rhs%particles(j)%rho = psys_rhs%particles(j)%rho + mw
-            psys_rhs%particles(j)%v(:) = psys_rhs%particles(j)%v(:) + psys_lhs%particles(i)%v(:)*vw
+            da = abs(ps_fluid(i)%x(2) - self%x)
+            db = abs(psys_rhs%particles(j)%x(2) - self%x)
+            vb(:) = -min(3._fp, db/da)*ps_fluid(i)%v(:)
+            call artificial_viscosity_monaghan1994( &
+                ps_fluid(i)%x(:), psys_rhs%particles(j)%x(:), ps_fluid(i)%v(:), vb(:), ps_fluid(i)%rho, &
+                ps_fluid(i)%rho, self%h, self%h, ps_fluid(i)%c, ps_fluid(i)%c, ps_fluid(i)%mass, ps_fluid(i)%mass, &
+                ps_fluid(i)%dvxdt(:), dummy_dvxdt(:), pairs%dwdx(:, k), self%artvisc_alpha, self%artvisc_beta &
+                )
+            call isotropic_pressure_force( &
+                ps_fluid(i)%p, ps_fluid(i)%p, ps_fluid(i)%rho, ps_fluid(i)%rho, ps_fluid(i)%mass, ps_fluid(i)%mass, &
+                ps_fluid(i)%dvxdt(:), dummy_dvxdt(:), pairs%dwdx(:, k) &
+                )
+            call continuity_density( &
+                ps_fluid(i)%v(:), vb(:), ps_fluid(i)%mass, ps_fluid(i)%mass, &
+                ps_fluid(i)%drhodt, dummy_drhodt, pairs%dwdx(:, k) &
+                )
         end do
 
-        do j = 1, psys_rhs%size
-            if (wsum(j) > 0._fp) then
-                psys_rhs%particles(j)%v(:) = -psys_rhs%particles(j)%v(:)/wsum(j)
-                psys_rhs%particles(j)%rho = psys_rhs%particles(j)%rho/wsum(j)
-            end if
-        end do
-
-    end subroutine boundary_update_sweep
+    end subroutine morris_boundary_sweep
 
 end module grasph_dambreak_ghost_morris_boundary_m
 
@@ -75,11 +81,11 @@ program main
     use grasph_kernels_m, only: cubic_bspline_kernel_t
 
     implicit none
-    type(particle_system_t):: psys(4)
-    type(system_interaction_t):: psys_interactions(4)
+    type(particle_system_t):: psys(5)
+    type(system_interaction_t):: psys_interactions(5)
     type(cubic_bspline_kernel_t):: kernel
     type(fluid_sweeper_t):: sweeper
-    type(boundary_update_sweeper_t):: boundary_sweeper
+    type(morris_boundary_sweeper_t):: boundary_sweeper
     type(xsph_shifter_t):: shifter
     type(tait_eos_state_updater_t):: state_updater
     type(eos_particle_t):: ps_template
@@ -136,26 +142,13 @@ program main
     ! only need to initialize metadata and position as only position is used to calculate repulsive force
     nlayer = ceiling(kernel%cutoff/dx)
 
-    call generate_boundary(psys(2), 25._fp, 40._fp)
+    call generate_boundary(psys(2), 25._fp, .true.)
+    call generate_boundary(psys(3), 25._fp, .false.)
 
     ghost_state_updater%surface_normal(:) = [1._fp, 0._fp]
-    call psys(3)%init(n=2500, name="ghost_boundary_left", particle_template=ghost_ps_template, state_updater=ghost_state_updater)
-    call psys(4)%init(n=2500, name="ghost_boundary_right", particle_template=ghost_ps_template, state_updater=ghost_state_updater)
+    call psys(4)%init(n=2500, name="ghost_boundary_left", particle_template=ghost_ps_template, state_updater=ghost_state_updater)
+    call psys(5)%init(n=2500, name="ghost_boundary_right", particle_template=ghost_ps_template, state_updater=ghost_state_updater)
     ! register variables for io
-    select type (p => psys(3)%particles)
-    class is (eos_ghost_particle_t)
-        call psys(3)%register_io%register_variable(p(1), "x", p(1)%x)
-        call psys(3)%register_io%register_variable(p(1), "v", p(1)%v)
-        call psys(3)%register_io%register_variable(p(1), "rho", p(1)%rho)
-        call psys(3)%register_io%register_variable(p(1), "mass", p(1)%mass)
-        call psys(3)%register_io%register_variable(p(1), "c", p(1)%c)
-        call psys(3)%register_io%register_variable(p(1), "dvxdt", p(1)%dvxdt)
-        call psys(3)%register_io%register_variable(p(1), "drhodt", p(1)%drhodt)
-        call psys(3)%register_io%register_variable(p(1), "p", p(1)%p)
-    class default
-        error stop "Expected eos_ghost_particle_t for psys(4)%p."
-    end select
-
     select type (p => psys(4)%particles)
     class is (eos_ghost_particle_t)
         call psys(4)%register_io%register_variable(p(1), "x", p(1)%x)
@@ -170,6 +163,20 @@ program main
         error stop "Expected eos_ghost_particle_t for psys(4)%p."
     end select
 
+    select type (p => psys(4)%particles)
+    class is (eos_ghost_particle_t)
+        call psys(5)%register_io%register_variable(p(1), "x", p(1)%x)
+        call psys(5)%register_io%register_variable(p(1), "v", p(1)%v)
+        call psys(5)%register_io%register_variable(p(1), "rho", p(1)%rho)
+        call psys(5)%register_io%register_variable(p(1), "mass", p(1)%mass)
+        call psys(5)%register_io%register_variable(p(1), "c", p(1)%c)
+        call psys(5)%register_io%register_variable(p(1), "dvxdt", p(1)%dvxdt)
+        call psys(5)%register_io%register_variable(p(1), "drhodt", p(1)%drhodt)
+        call psys(5)%register_io%register_variable(p(1), "p", p(1)%p)
+    class default
+        error stop "Expected eos_ghost_particle_t for psys(4)%p."
+    end select
+
     sweeper%artvisc_alpha = 0.01_fp
     sweeper%artvisc_beta = 0._fp
     sweeper%g = g
@@ -179,17 +186,24 @@ program main
     shifter%epsilon = 0.5_fp
     shifter%update_rhs = .false.
 
+    boundary_sweeper%artvisc_alpha = 0.01_fp
+    boundary_sweeper%artvisc_beta = 0._fp
+    boundary_sweeper%h = 1.2_fp*dx
+    boundary_sweeper%x = 0._fp
+
     ! init interactions
     call psys_interactions(1)%init(30, psys(1), sweeper=sweeper, shifter=shifter)
     sweeper%initialize = .false. ! second sweeper doesn't need to zero acceleration arrays
-    call psys_interactions(2)%init(30, psys(1), psys(2), prologue_sweeper=boundary_sweeper, sweeper=sweeper, shifter=shifter)
+    call psys_interactions(2)%init(30, psys(1), psys(2), sweeper=boundary_sweeper, shifter=shifter)
+    boundary_sweeper%x = 40._fp
+    call psys_interactions(3)%init(30, psys(1), psys(3), sweeper=boundary_sweeper, shifter=shifter)
     ghost_timestep_setuper%cutoff = kernel%cutoff
     ghost_timestep_setuper%surface_normal(:) = [1._fp, 0._fp]
     ghost_timestep_setuper%point(:) = [0._fp, 0._fp]
-    call psys_interactions(3)%init(30, psys(1), psys(3), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
+    call psys_interactions(4)%init(30, psys(1), psys(4), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
     ghost_timestep_setuper%surface_normal(:) = [-1._fp, 0._fp]
     ghost_timestep_setuper%point(:) = [25._fp, 0._fp]
-    call psys_interactions(4)%init(30, psys(1), psys(4), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
+    call psys_interactions(5)%init(30, psys(1), psys(5), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
 
     ! start time-evolution with damping for setting up of initial conditions for fluid.
     call leap_frog_time_integration( &
@@ -207,11 +221,12 @@ program main
         )
 
     ! re-initialize boundary conditions so that geometry matches the dambreak setup (without ramp).
-    call generate_boundary(psys(2), 75._fp, 40._fp)
+    call generate_boundary(psys(2), 75._fp, .true.)
+    call generate_boundary(psys(3), 75._fp, .false.)
 
     ghost_timestep_setuper%cutoff = kernel%cutoff
     ghost_timestep_setuper%point(:) = [75._fp, 0._fp]
-    call psys_interactions(4)%init(30, psys(1), psys(4), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
+    call psys_interactions(5)%init(30, psys(1), psys(5), timestep_setuper=ghost_timestep_setuper, sweeper=sweeper)
 
     ! start time-evolution what dambreak setup and without damping.
     call leap_frog_time_integration( &
@@ -228,56 +243,47 @@ program main
 
 contains
 
-    subroutine generate_boundary(psys_boundary, extx, exty)
+    subroutine generate_boundary(psys_boundary, extx, bottom)
 
         type(particle_system_t), intent(out):: psys_boundary
-        real(fp), intent(in):: extx, exty
-        integer:: nbx, nby, nvirt
+        real(fp), intent(in):: extx
+        logical, intent(in):: bottom
+        integer:: nbx, nvirt
+        type(base_particle_t):: p
 
         nbx = extx/dx
-        nby = exty/dx
 
-        nvirt = 2*nlayer*nbx + 4*nlayer*nlayer
-
-        call psys_boundary%init(nvirt, name="boundary", state_updater=state_updater, particle_template=ps_template)
-
-        select type (p => psys_boundary%particles)
-        class is (eos_particle_t)
-
-            call psys_boundary%register_io%register_variable(p(1), "x", p(1)%x)
-            call psys_boundary%register_io%register_variable(p(1), "v", p(1)%v)
-            call psys_boundary%register_io%register_variable(p(1), "rho", p(1)%rho)
-            call psys_boundary%register_io%register_variable(p(1), "mass", p(1)%mass)
-            call psys_boundary%register_io%register_variable(p(1), "c", p(1)%c)
-            call psys_boundary%register_io%register_variable(p(1), "dvxdt", p(1)%dvxdt)
-            call psys_boundary%register_io%register_variable(p(1), "drhodt", p(1)%drhodt)
-            call psys_boundary%register_io%register_variable(p(1), "p", p(1)%p)
-        end select
+        nvirt = nlayer*nbx + 2*nlayer*nlayer
 
         k = 0
-        ! bottom layer and corners
-        do i = -nlayer, nbx + nlayer - 1
-            do j = 0, nlayer - 1
-                k = k + 1
-                psys(2)%particles(k)%x(1) = (i + 0.5_fp)*dx
-                psys(2)%particles(k)%x(2) = -(j + 0.5_fp)*dx
+        if (bottom) then
+            ! bottom layer and corners
+            call psys_boundary%init(nvirt, name="bottom_boundary")
+            do i = -nlayer, nbx + nlayer - 1
+                do j = 0, nlayer - 1
+                    k = k + 1
+                    psys_boundary%particles(k)%x(1) = (i + 0.5_fp)*dx
+                    psys_boundary%particles(k)%x(2) = -(j + 0.5_fp)*dx
+                end do
             end do
-        end do
-        ! top layer and corners
-        do i = -nlayer, nbx + nlayer - 1
-            do j = 0, nlayer - 1
-                k = k + 1
-                psys(2)%particles(k)%x(1) = (i + 0.5_fp)*dx
-                psys(2)%particles(k)%x(2) = exty + (j + 0.5_fp)*dx
+        else
+            ! top layer and corners
+            call psys_boundary%init(nvirt, name="top_boundary")
+            do i = -nlayer, nbx + nlayer - 1
+                do j = 0, nlayer - 1
+                    k = k + 1
+                    psys_boundary%particles(k)%x(1) = (i + 0.5_fp)*dx
+                    psys_boundary%particles(k)%x(2) = 40._fp + (j + 0.5_fp)*dx
+                end do
             end do
-        end do
+        end if
         do i = 1, k
-            psys(2)%particles(i)%id = i
-            psys(2)%particles(i)%type = -1
-            psys(2)%particles(i)%rho = rho0
-            psys(2)%particles(i)%mass = rho0*dx*dx
-            psys(2)%particles(i)%c = 10._fp*sqrt(490.5_fp)
+            psys_boundary%particles(i)%id = i
+            psys_boundary%particles(i)%type = -1
         end do
+
+        psys_boundary%to_print_summary = .false.
+        call psys_boundary%register_io%register_variable(psys_boundary%particles(1), "x", psys_boundary%particles(1)%x)
 
     end subroutine generate_boundary
 
